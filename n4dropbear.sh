5c06620912016e374918beeddbca0260a017f1ae
N4ND4 <47235064+Nanda-N4@users.noreply.github.com>
2022-12-23 16:11:51 +0630

    Update n4dropbear.sh

--- n4dropbear.sh
+++ n4dropbear.sh
@@ -1,22 +1,40 @@
-apt install dropbear -y
-apt install cmake -y
-apt install unzip -y
-apt install screen -y
-apt install wget -y
-apt install python -y
-wget https://github.com/ambrop72/badvpn/archive/master.zip
-unzip master.zip
-cd badvpn-master
-mkdir build
-cd build
-cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1;make install;
-wget https://raw.githubusercontent.com/Nanda-N4/db/main/dropbear.conf
-wget https://raw.githubusercontent.com/mgtortoise/pdme/main/main.py
-wget https://raw.githubusercontent.com/mgtortoise/pdme/main/websocket.service
-mv main.py websocket.service /etc/systemd/system/
-rm -rf /etc/default/dropbear
-mv dropbear /etc/default/dropbear
-/etc/init.d/dropbear restart
-systemctl restart websocket.service
-systemctl enable websocket.service
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+#! /bin/bash
+#
+# Coded by Khai Phan
+#
+# Email: khaiphan9x@gmail.com
+#
+# Auto installer script for 
+# dropbear
+#
+#
+
+clear
+cd /tmp
+echo "Ban dang su dung script tu dong cai dat Dropbear cho Debian va Ubuntu duoc viet boi Khai Phan"
+echo "================================================="
+echo "" 
+echo "Dang cap nhat tai nguyen..."
+apt-get update  > /dev/null 2>&1
+echo ""
+echo "Bat dau cai dat Dropbear"
+echo ""
+apt-get -y install dropbear ssh
+sed -i 's/NO_START\=1/NO_START\=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT\=22/DROPBEAR_PORT\=442/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS\=/DROPBEAR_EXTRA_ARGS\=\"\-p 442 \-p 80 \-p 8080 \-p 8484 \-p 143 \-p 109\"/g' /etc/default/dropbear
+sed -i 's/#PermitRootLogin prohibit\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin without\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service dropbear start
+service sshd restart
+echo ""
+echo ""
+echo "Da hoan thanh cai dat Dropbear"
+echo "Dropbear dang chay tren port: 442, 80, 8080, 8484, 143, 109"
+echo "Hay tao 1 user moi de su dung"
+echo ""
+echo "Dang khoi dong lai server..."
+echo ""
+sleep 5 ; reboot
